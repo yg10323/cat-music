b7844b239af25b07660badaf71307b2283da31cb
yg10323 <1357373713@qq.com>
2022-01-10 18:48:06 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+export default memo(function App() {
+  return (
+    <div>
+      <h2>Cat Music</h2>
+    </div>
+  )
+})
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import "./assets/style/base.css"
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
