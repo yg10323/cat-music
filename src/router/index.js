58f62a706e6c6f4c0aac57caf725746b487bcccb
yg10323 <1357373713@qq.com>
2022-01-11 11:34:35 +0800

    副导航菜单

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,13 @@
 import { Redirect } from "react-router-dom";
 
 import CatDiscover from '@/views/discover'
+import CatRecommend from '@/views/discover/web/recommend'
+import CatRanking from '@/views/discover/web/ranking'
+import CatSongs from '@/views/discover/web/songs'
+import CatDjradio from '@/views/discover/web/djradio'
+import CatArtist from '@/views/discover/web/artist'
+import CatAlbum from '@/views/discover/web/album'
+
 import CatSelf from '../views/self'
 import CatFollow from '../views/follow'
 
@@ -16,7 +23,45 @@ const routes = [
   },
   {
     path: '/discover',
-    component: CatDiscover
+    component: CatDiscover,
+    routes: [
+      {
+        path: "/discover",
+        exact: true,
+        render: () => (
+          <Redirect to="/discover/recommend" />
+        )
+      },
+      {
+        path: "/discover/recommend",
+        component: CatRecommend
+      },
+      {
+        path: "/discover/ranking",
+        component: CatRanking
+      },
+      {
+        path: "/discover/songs",
+        component: CatSongs
+      },
+      {
+        path: "/discover/djradio",
+        exact: true,
+        component: CatDjradio
+      },
+      {
+        path: "/discover/artist",
+        component: CatArtist
+      },
+      {
+        path: "/discover/album",
+        component: CatAlbum
+      },
+      // {
+      //   path: "/discover/player",
+      //   component: HYPlayer
+      // }
+    ]
   },
   {
     path: '/self',
--- src/views/discover/index.js
+++ src/views/discover/index.js
@@ -1,9 +1,34 @@
-import React, { memo } from 'react'
+import React, { memo } from 'react';
+import { renderRoutes } from 'react-router-config';
 
-export default memo(function CatDiscover() {
+import { dicoverMenu } from "@/common/local-data";
+
+import { NavLink } from 'react-router-dom';
+import {
+  DiscoverWrapper,
+  TopMenu
+} from './style';
+
+export default memo(function CatDiscover(props) {
+  const { route } = props;
+
   return (
-    <div>
-      <h2>CatDiscover</h2>
-    </div>
+    <DiscoverWrapper>
+      <div className="top">
+        <TopMenu className="wrap-v1">
+          {
+            dicoverMenu.map((item, index) => {
+              return (
+                <div className="item" key={item.title}>
+                  <NavLink to={item.link}>{item.title}</NavLink>
+                </div>
+              )
+            })
+          }
+        </TopMenu>
+      </div>
+      {renderRoutes(route.routes)}
+    </DiscoverWrapper>
   )
 })
+
